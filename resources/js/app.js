65058c9aafd38c2f069335ab17e1a4c7a83b5d01
Ikaro Laborda <iclaborda@msn.com>
2022-11-17 22:47:39 +0000

    feat: implementing resource and pagination

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,8 @@ require('./bootstrap');
 import {createApp} from "vue";
 import PostsIndex from "./components/Posts/Index";
 
-const app = createApp({});
-app.component('posts-index', PostsIndex);
+
+const app = createApp({})
+app.component('posts-index', PostsIndex)
 app.mount('#app');
 
--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,28 +1,31 @@
 <template>
     <div class="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
         <div class="min-w-full align-middle">
-            <table class="min-w-full divide-y divide-gray-200 border">
+            <table class="border border-solid w-full">
                 <thead>
                 <tr>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span></th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span></th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span></th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created At</span></th>
+                    <th class="px-6 py-3 bg-gray-100 border text-left">
+                        <span class="text-xs leading-3 font-medium text-gray-500 uppercase tracking-wider">ID</span></th>
+                    <th class="px-6 py-3 bg-gray-100 border text-left">
+                        <span class="text-xs leading-3 font-medium text-gray-500 uppercase tracking-wider">Title</span></th>
+                    <th class="px-6 py-3 bg-gray-100 border text-left">
+                        <span class="text-xs leading-3 font-medium text-gray-500 uppercase tracking-wider">Content</span></th>
+                    <th class="px-6 py-3 bg-gray-100 border text-left">
+                        <span class="text-xs leading-3 font-medium text-gray-500 uppercase tracking-wider">Created At</span></th>
                 </tr>
                 </thead>
-                <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                <tr v-for="post in posts">
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.id }}</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.title }}</td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.content }}</td>
-                    <td cclass="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">{{ post.created_at }}</td>
+                <tbody class="">
+                <tr v-for="post in posts.data">
+                    <td class="border divide-opacity-50 divide-x-2 divide-y-2 px-6 py-4 whitespace-no-wrap leading-5 text-sm text-gray-900">{{ post.id }}</td>
+                    <td class="border divide-opacity-50 divide-x-2 divide-y-2 px-6 py-4 whitespace-no-wrap leading-5 text-sm text-gray-900">{{ post.title }}</td>
+                    <td class="border divide-opacity-50 divide-x-2 divide-y-2 px-6 py-4 whitespace-no-wrap leading-5 text-sm text-gray-900">{{ post.content }}</td>
+                    <td class="border divide-opacity-50 divide-x-2 divide-y-2 px-6 py-4 whitespace-no-wrap leading-5 text-sm text-gray-900">{{ post.created_at }}</td>
                 </tr>
                 </tbody>
             </table>
+            <div class="mx-auto min-w-full align-middle mt-4">
+                <TailwindPagination :data="posts" @pagination-change-page="getPosts" />
+            </div>
         </div>
     </div>
 </template>
@@ -30,12 +33,16 @@
 <script>
 import {onMounted} from "vue";
 import usePosts from "../../composables/posts";
+import { TailwindPagination } from 'laravel-vue-pagination';
 export default {
+    components: {
+        TailwindPagination
+    },
     setup() {
         const { posts, getPosts } = usePosts();
         onMounted(getPosts);
-        return { posts };
-    },
-    name: "PostsIndex",
+
+        return { posts, getPosts };
+    }
 };
 </script>
